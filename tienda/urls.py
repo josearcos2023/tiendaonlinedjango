1beda2a300d8d39908b9c47b6a7f51a4d20386c4
César Mayta <cesarmayta@gmail.com>
2023-05-10 12:05:01 -0500

    api

--- tienda/urls.py
+++ tienda/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'tienda'
+
 urlpatterns = [
     path('', views.index),
     path('producto',views.producto),
-    path('categoria/<int:categoria_id>',views.categoria)
+    path('categoria/<int:categoria_id>',views.categoria,name='categoria')
 ]
